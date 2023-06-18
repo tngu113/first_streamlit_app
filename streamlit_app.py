f266a53f87ca2e3511d24316cf50d389f9ebd699
tngu113 <135894130+tngu113@users.noreply.github.com>
2023-06-18 17:28:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,4 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 #Allow the end user to add a fruit to the lost
-streamlit.write("Thanks for adding ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
