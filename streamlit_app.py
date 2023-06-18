eff4bf4de8176d144560f50329679e2ac1365c46
tngu113 <135894130+tngu113@users.noreply.github.com>
2023-06-18 17:38:38 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output if the screen as table
 streamlit.dataframe(fruityvice_normalized)
-"don' run anything past here while we troubleshoot
+# don't run anything past here while we troubleshoot
 streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
