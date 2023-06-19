6ad9650f9029fc59fca9c6a71e7259e96a1a06e8
tngu113 <135894130+tngu113@users.noreply.github.com>
2023-06-18 19:38:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,7 @@ try:
 
 except URLError as e:
     streamlit.error()
-# don't run anything past here while we troubleshoot
+#don't run anything past here while we troubleshoot
 #streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
